8b7a1b313b704860f06d7f3af8414130e841cfe8
bhuwan-darai <dr.darai29@gmail.com>
2024-03-22 22:37:55 +0545

    frontend initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <button className=" border border-slate-400 ml-[10px] mt-[20px] rounded-lg">
+        Click me !!!
+      </button>
+    </div>
+  );
+}
+
+export default App;
