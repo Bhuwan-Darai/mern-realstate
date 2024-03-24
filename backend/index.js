f57f2b58fdc1d2df52394bb10ae73b2978ef8f32
bhuwan-darai <dr.darai29@gmail.com>
2024-03-24 23:01:00 +0545

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(
+    console.log(() => {
+      console.log("connection failed");
+    })
+  );
 
 const app = express();
 
